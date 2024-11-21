6b42ea8b3353826e4be157a39e053d89efd2b08f
rhagerty32 <ry2402@gmail.com>
2024-11-21 12:42:48 -0700

    update port for deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require("path");
 let session = require('express-session');
 // Import the pages get routes from pageRoutes.js (for navigation between pages)
 let pagesRouter = require("./routes/pageRoutes");
-const port = 5001;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
