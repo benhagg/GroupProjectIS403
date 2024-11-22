8d4f2b2e39b252d538df4a5183a0ca0693ac1d19
rhagerty32 <ry2402@gmail.com>
2024-11-21 18:14:35 -0700

    adjust link

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d bakery.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email ryguy32@byu.edu
+sudo certbot -n -d bakery.is404.net --nginx --agree-tos --email ryguy32@byu.edu
